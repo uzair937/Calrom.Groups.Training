9fdec7b64a2a7096da0cba7dd558e78f7de3a8e6
Will Booth <willbooth97@hotmail.co.uk>
2019-10-30 16:57:12 +0000

    Organised the code into prototypes

--- Html/ToDoList/todolist.js
+++ Html/ToDoList/todolist.js
@@ -1,29 +1,50 @@
 window.addEventListener('load', run);
-var checknum = 0;
+var checkNum;
 var dragSrcEl = null;
-var dragOn;
-var dropOn;
-
-function checkPosition() {}
-
-checkPosition.prototype.aboveOrBelow = function (dragSrcElt) {
-    for (var i = 0; i < event.currentTarget.parentNode.children.length - 1; i++) {
-        if (event.currentTarget === event.currentTarget.parentNode.children[i]) window.dropOn = i;
-        if (dragSrcElt === event.currentTarget.parentNode.children[i]) window.dragOn = i;
-    }
-}
 
 function run() {
-    var el = document.getElementById("enter-task");
-    if (!el) return;
-    var delel = document.getElementById("delete-task");
-    var botel = document.getElementById("bottom-list");
-    addDnDHandlerBottom(botel);
-    el.addEventListener("submit", addTask);
-    delel.addEventListener("submit", delTask);
+    checkNum = 0;
+    var eventManager = new EventManager();
+    eventManager.addSubmitEvents();
+    var dragManager = new DragManager();
+    dragManager.addDnDBottom();
 }
 
-function delTask() {
+function TaskBuilder() { }
+
+TaskBuilder.prototype.createCheckbox = function () {
+    var check = document.createElement('input');
+    check.setAttribute("type", "checkbox");
+    return check;
+}
+
+TaskBuilder.prototype.createContainer = function () {
+    var taskManager = new TaskManager();
+    var li = document.createElement("div");
+    li.setAttribute("draggable", "true");
+    li.setAttribute("id", "box" + window.checkNum);
+    return li;
+}
+
+TaskBuilder.prototype.createTextElement = function () {
+    var textelement = document.createElement("p");
+    var inputValue = document.getElementById("input-task").value;
+    var t = document.createTextNode(inputValue);
+    textelement.appendChild(t);
+    textelement.setAttribute("class", "task-text");
+    document.getElementById("input-task").value = '';
+    return textelement;
+}
+
+TaskBuilder.prototype.buildTask = function (tEl, li, check) {
+    li.appendChild(check);
+    li.appendChild(tEl);
+    return li;
+}
+
+function TaskManager() { }
+
+TaskManager.prototype.deleteTasks = function () {
     event.preventDefault();
     var delList = document.getElementsByClassName("deletable");
     if (delList) {
@@ -31,100 +52,182 @@ function delTask() {
     }
 }
 
-function addTask() {
+TaskManager.prototype.addTask = function () {
     event.preventDefault();
-    var li = document.createElement("div");
-    var textelement = document.createElement("p");
-    var inputValue = document.getElementById("input-task").value;
-    var check = document.createElement('input');
-    var t = document.createTextNode(inputValue);
-    li.setAttribute("draggable", "true");
-    li.setAttribute("id", "box" + checknum);
-    check.setAttribute("type", "checkbox");
-    textelement.appendChild(t);
-    textelement.setAttribute("class", "task-text");
-    li.appendChild(check);
-    li.appendChild(textelement);
-    if (inputValue === '') alert("Enter a task");
-    else {
-        var botel = document.getElementById("bottom-list");
-        botel.insertAdjacentElement('beforebegin', li);
-    }
-    document.getElementById("input-task").value = '';
+    var taskBuilder = new TaskBuilder();
+    var taskManager = new TaskManager();
+    var eventManager = new EventManager();
+    var tEl = taskBuilder.createTextElement();
+    var li = taskBuilder.createContainer();
+    var check = taskBuilder.createCheckbox();
+    li = taskBuilder.buildTask(tEl, li, check);
+    eventManager.addListeners(li);
+    taskManager.drawTask(li);
+    window.checkNum++;
+    return li;
+}
 
-    eC = li.firstChild;
+TaskManager.prototype.drawTask = function (li) {
+    var botel = document.getElementById("bottom-list");
+    botel.insertAdjacentElement('beforebegin', li);
+}
+
+function EventManager() {
+    this.el = document.getElementById("enter-task");
+    if (!this.el) return;
+    this.delel = document.getElementById("delete-task");
+    this.botel = document.getElementById("bottom-list");
+}
+
+EventManager.prototype.pickDropMethod = function () {
+    var dragOn, dropOn;
+    for (var i = 0; i < event.currentTarget.parentNode.children.length; i++) {
+        if (event.currentTarget === event.currentTarget.parentNode.children[i]) dropOn = i;
+        if (window.dragSrcEl === event.currentTarget.parentNode.children[i]) dragOn = i;
+    }
+    return (dropOn - dragOn);
+}
+
+EventManager.prototype.addSubmitEvents = function () {
+    var taskManager = new TaskManager();
+    this.el.addEventListener("submit", taskManager.addTask);
+    this.delel.addEventListener("submit", taskManager.deleteTasks);
+}
+
+EventManager.prototype.addListeners = function (li) {
     if (li) {
+        var dragManager = new DragManager();
+        var taskEditor = new TaskEditor();
+        eC = li.firstChild;
         var eT = li.getElementsByClassName('task-text')[0];
         eC.addEventListener("click", checkBox);
-        eT.addEventListener("click", editText);
-        addDnDHandlers(li);
+        eT.addEventListener("click", taskEditor.textToInput);
+        dragManager.addDnDHandlers(li);
     }
-    checknum++;
 }
 
-function addDnDHandlers(li) {
-    li.addEventListener("dragstart", drag);
-    li.addEventListener("dragover", dragover);
-    li.addEventListener("drop", drop);
+EventManager.prototype.escapeEdit = function () {
+    if (event.key === "Escape") {
+        eT = event.currentTarget.parentNode.getElementsByClassName('task-text')[0];
+        eT.setAttribute("style", "display: block;");
+        event.currentTarget.parentNode.getElementsByClassName("edit-form")[0].remove();
+    }
 }
 
-function addDnDHandlerBottom(li) {
-    li.addEventListener("dragover", dragover);
-    li.addEventListener("drop", drop);
+function DragManager() { }
+
+DragManager.prototype.addDnDHandlers = function (li) {
+    var dragEvents = new DragEvents();
+    li.addEventListener("dragstart", dragEvents.drag);
+    li.addEventListener("dragover", dragEvents.dragover);
+    li.addEventListener("drop", dragEvents.drop);
 }
 
-function dragover() {
+DragManager.prototype.addDnDBottom = function () {
+    var dragEvents = new DragEvents();
+    var botel = document.getElementById("bottom-list");
+    botel.addEventListener("dragover", dragEvents.dragover);
+    botel.addEventListener("drop", dragEvents.drop);
+}
+
+function DragEvents() { }
+
+DragEvents.prototype.dragover = function () {
     event.preventDefault();
     for (var i = 0; i < event.currentTarget.parentNode.children.length - 1; i++) {
         event.currentTarget.parentNode.children[i].setAttribute("style", "border: 0px")
         event.currentTarget.parentNode.children[i].setAttribute("style", "border-bottom: 3px solid black")
     }
     event.currentTarget.setAttribute("style", "border-bottom: 3px solid blue;")
-    event.currentTarget.parentNode.children[event.currentTarget.parentNode.children.length-1].setAttribute("style", "border-bottom: 0;");
+    event.currentTarget.parentNode.children[event.currentTarget.parentNode.children.length - 1].setAttribute("style", "border-bottom: 0;");
 }
 
-function drag() {
-    dragSrcEl = event.currentTarget;
+DragEvents.prototype.drag = function () {
+    window.dragSrcEl = event.currentTarget;
 }
 
-function drop() {
+DragEvents.prototype.drop = function () {
     event.preventDefault();
+    var eventManager = new EventManager();
+    var dragManager = new DragManager();
     if (event.stopPropagation) event.stopPropagation();
-    var dropProto = new checkPosition();
-    dropProto.aboveOrBelow(dragSrcEl);
-    if (dragSrcEl != event.currentTarget) {
-        if (window.dropOn - window.dragOn > 0) {
+    if (window.dragSrcEl != event.currentTarget) {
+        if (eventManager.pickDropMethod() > 0) {
             event.currentTarget.parentNode.removeChild(dragSrcEl);
             var dropHTML = dragSrcEl;
             event.currentTarget.insertAdjacentElement('afterend', dropHTML);
-            var dropElem = event.currentTarget.previousSibling;
-            addDnDHandlers(dropElem);
-            if (dropElem.className === "deletable") {
-                dropElem.firstChild.checked = true;
-            }
-            for (var i = 0; i < event.currentTarget.parentNode.children.length - 1; i++) {
-                event.currentTarget.parentNode.children[i].setAttribute("style", "border: 0px")
-                event.currentTarget.parentNode.children[i].setAttribute("style", "border-bottom: 3px solid black")
-            }
         }
         else {
             event.currentTarget.parentNode.removeChild(dragSrcEl);
             var dropHTML = dragSrcEl;
             event.currentTarget.insertAdjacentElement('beforebegin', dropHTML);
-            var dropElem = event.currentTarget.previousSibling;
-            addDnDHandlers(dropElem);
-            if (dropElem.className === "deletable") {
-                dropElem.firstChild.checked = true;
-            }
-            for (var i = 0; i < event.currentTarget.parentNode.children.length - 1; i++) {
-                event.currentTarget.parentNode.children[i].setAttribute("style", "border: 0px")
-                event.currentTarget.parentNode.children[i].setAttribute("style", "border-bottom: 3px solid black")
-            }
+        }
+        var dropElem = event.currentTarget.previousSibling;
+        dragManager.addDnDHandlers(dropElem);
+        if (dropElem.className === "deletable") {
+            dropElem.firstChild.checked = true;
+        }
+        for (var i = 0; i < event.currentTarget.parentNode.children.length - 1; i++) {
+            event.currentTarget.parentNode.children[i].setAttribute("style", "border: 0px")
+            event.currentTarget.parentNode.children[i].setAttribute("style", "border-bottom: 3px solid black")
         }
     }
 }
 
-function editText() {
+function TaskEditor() { }
+
+TaskEditor.prototype.textToInput = function () {
+    event.preventDefault();
+    var editManager = new EditManager();
+    var formInput = editManager.createEditInput()
+    editManager.addInputListeners(formInput);
+    editManager.drawTask(formInput);
+    editManager.focusOnInput(formInput);
+}
+
+TaskEditor.prototype.inputToText = function () {
+    event.preventDefault();
+    var eventManager = new EventManager();
+    var editManager = new EditManager();
+    var li = editManager.createNewTask()
+    eventManager.addListeners(li);
+    editManager.removeOldInput(li);
+}
+
+function EditManager() { }
+
+EditManager.prototype.removeOldInput = function (li) {
+    li.getElementsByClassName("edit-form")[0].remove();
+    if (li.className === "deletable") checkBox();
+}
+
+EditManager.prototype.createNewTask = function () {
+    var li = event.currentTarget.parentNode;
+    var eT = li.getElementsByClassName('task-text')[0];
+    var newText = li.getElementsByClassName("edit-form")[0].value;
+    eT.innerText = newText;
+    eT.setAttribute("style", "display: block;");
+    return li;
+}
+
+EditManager.prototype.focusOnInput = function (formInput) {
+    var editInput = formInput.getElementsByClassName("edit-form")[0];
+    editInput.focus();
+    editInput.select();
+}
+
+EditManager.prototype.drawTask = function (newDiv) {
+    event.currentTarget.parentNode.appendChild(newDiv);
+}
+
+EditManager.prototype.addInputListeners = function (formInput) {
+    var eventManager = new EventManager();
+    var taskEditor = new TaskEditor();
+    formInput.addEventListener("submit", taskEditor.inputToText);
+    formInput.addEventListener("keyup", eventManager.escapeEdit);
+}
+
+EditManager.prototype.createEditInput = function () {
     var li = event.currentTarget.parentNode;
     eT = li.getElementsByClassName('task-text')[0];
     var editInput = document.createElement("input");
@@ -135,34 +238,7 @@ function editText() {
     editInput.setAttribute("value", previousText);
     formInput.appendChild(editInput);
     eT.setAttribute("style", "display: none;");
-    formInput.addEventListener("submit", addEditTask);
-    formInput.addEventListener("keyup", escapeEdit);
-    li.appendChild(formInput);
-    editInput.focus();
-    editInput.select();
-}
-
-function escapeEdit() {
-    if (event.key === "Escape") {
-        eT = event.currentTarget.parentNode.getElementsByClassName('task-text')[0];
-        eT.setAttribute("style", "display: block;");
-        event.currentTarget.parentNode.getElementsByClassName("edit-form")[0].remove();
-    }
-}
-
-function addEditTask() {
-    event.preventDefault();
-    var li = event.currentTarget.parentNode;
-    var newText = li.getElementsByClassName("edit-form")[0].value;
-    var textelement = document.createElement("p");
-    var t = document.createTextNode(newText);
-    textelement.setAttribute("class", "task-text");
-    textelement.appendChild(t);
-    textelement.addEventListener("click", editText);
-    debugger;
-    li.appendChild(textelement);
-    li.getElementsByClassName("edit-form")[0].remove();
-    if (li.className === "deletable") checkBox();
+    return formInput;
 }
 
 function checkBox() {
