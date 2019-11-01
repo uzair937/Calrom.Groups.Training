9bc4794d5e11b809ebf47202129ed4d8001da25e
Uzair <ufoolat@calrom.com>
2019-11-01 11:55:42 +0000

    QoL changes

--- Web Dev/ReactTraining/src/ToDoListApplication.js
+++ Web Dev/ReactTraining/src/ToDoListApplication.js
@@ -3,36 +3,36 @@ import { ToDoListForm } from '../src/ToDoListForm'
 import { ToDoListItem } from '../src/ToDoListItem'
 
 export class ToDoListApplication extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			items: []
-		};
-	}
+        this.state = {
+            items: []
+        };
+    }
 
-	onFormSubmit(text) {
-		this.setState({
-			items: [...this.state.items, text]
-		});
-	}
+    onFormSubmit(text) {
+        this.setState({
+            items: [...this.state.items, text]
+        });
+    }
 
-	onFormEdit(text, key) {
-		var array = this.state.items;
-		array[key] = text;
+    onFormEdit(text, key) {
+        var array = this.state.items;
+        array[key] = text;
 
-		this.setState({
-			items: array
-		});
-	}
+        this.setState({
+            items: array
+        });
+    }
 
-	render() {
-		return <div>
-			<ToDoListForm formSubmitted={(text) => this.onFormSubmit(text)} />
+    render() {
+        return <div>
+            <ToDoListForm formSubmitted={(text) => this.onFormSubmit(text)} />
 
-			{this.state.items.map((item, index) => {
-				return <ToDoListItem formSubmitted={(text, key) => this.onFormEdit(text, key)} itemValue={item} key={index} value = {index}/>
-			})}
-		</div>
-	}
+            {this.state.items.map((item, index) => {
+                return <ToDoListItem formSubmitted={(text, key) => this.onFormEdit(text, key)} itemValue={item} key={index} value={index} />
+            })}
+        </div>
+    }
 }
